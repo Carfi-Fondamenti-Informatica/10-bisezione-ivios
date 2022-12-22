524203f28b0e5b9410fc3e8a1e42e862b2a7f34e
Ivio Feng <115487784+ivios@users.noreply.github.com>
2022-12-22 22:15:49 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include <cmath>
+#include <iomanip>
+
 using namespace std;
 
+float valFunzione(float x) {
+    float funzione = (pow(x, 2) * cos(x)) + 1;
+    return funzione;
+}
+
 int main() {
-   return 0;
+    float a, b, x;
+    do {
+        cout << "inserire estremi";
+        cin >> a >> b;
+    } while (valFunzione(a) * valFunzione(b) >= 0);
+    
+    float err = abs(((b - a) / 2));
+
+    do {
+        x = ((a + b) / 2);
+        if (valFunzione(x) == 0) {
+            cout << setprecision(5) << x;
+            cout << setprecision(5) << valFunzione(x);
+            break;
+        } else {
+            if (valFunzione(a) * valFunzione(b) < 0) {
+                a = x;
+            } else {
+                b = x;
+            }
+        }
+    } while (err >= pow(1, -6));
+
+    return 0;
 }
